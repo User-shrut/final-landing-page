be78b977db73f2f0837f7485182f657fdc1c1733
User-shrut <160940967+User-shrut@users.noreply.github.com>
2024-05-07 22:55:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
-// var sheetName = 'Sheet1'
-// var scriptProp = PropertiesService.getScriptProperties()
 
-// function intialSetup () {
-//   var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-//   scriptProp.setProperty('key', activeSpreadsheet.getId())
-// }
-
-// function doPost (e) {
-//   var lock = LockService.getScriptLock()
-//   lock.tryLock(10000)
-
-//   try {
-//     var doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
-//     var sheet = doc.getSheetByName(sheetName)
-
-//     var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-//     var nextRow = sheet.getLastRow() + 1
-
-//     var newRow = headers.map(function(header) {
-//       return header === 'timestamp' ? new Date() : e.parameter[header]
-//     })
-
-//     sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
-
-//     return ContentService
-//       .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
-//       .setMimeType(ContentService.MimeType.JSON)
-//   }
-
-//   catch (e) {
-//     return ContentService
-//       .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-//       .setMimeType(ContentService.MimeType.JSON)
-//   }
-
-//   finally {
-//     lock.releaseLock()
-//   }
-// }
-// //https://script.google.com/macros/s/AKfycbzkbDI_1606fVi9-nbrHCpNrnDYM2jTDi0jVKPqlIVPWWdpe0ipr2W5tMa0bTfwio1O/exec
-
-const scriptURL='https://script.google.com/macros/s/AKfycbw4SGzOYnezHnCj6HpwwkdUVqG431LP8Q6v63QRJprgjYt7C0OH5BQ16ds1BY6SriMG/exec'
+const scriptURL='https://script.google.com/macros/s/AKfycbyjDqdf1CAQjWTHP96ov-WKujkLwnn_3dIKEidhVe-5HrOvyOEMZcL_LYmTIwV2z3N7RA/exec'
 
 const form = document.forms['google-sheet'];
 
@@ -59,13 +18,6 @@ form.addEventListener('submit', e => {
         })
         .catch(error => console.error('Error!', error.message));
 });
-
-
-
-
-
-
-
 
 document.querySelector(".hamburger").addEventListener("click", () => {
     let a=document.querySelector(".navlist");
@@ -94,17 +46,7 @@ function closeFullScreen() {
     var fullscreenContainer = document.getElementsByClassName("fullscreen-container")[0];
     fullscreenContainer.style.display = "none";
 }
-// document.addEventListener("DOMContentLoaded", function() {
-//     document.getElementById('overlay').style.display = 'block';
-// });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     document.getElementById('overlay').style.display = 'block';
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     document.getElementById('overlay').style.display = 'block';
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.forms['google-sheet1'];   
